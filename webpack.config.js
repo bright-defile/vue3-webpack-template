b2d88e28dbecebdfc59671b049ea09d6aea08f83
bright-defile <raspberrypichef191@gmail.com>
2021-07-07 13:35:55 +0900

    prettier-add

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+// export
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  resolve: {
+    extensions: [".js", ".vue"],
+    // 경로 별칭
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+      assets: path.resolve(__dirname, "src/assets"),
+    },
+  },
+  // parcel index.html 같이
+  // 파일 읽어들이기 시작하는 진입점
+  // 웹팩은 기본이 js로 진입
+  entry: "./src/main.js",
+  // 결과물(번들)을 변환하는 설정
+  output: {
+    // path: path.resolve(__dirname, "dist"),
+    // filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "vue-style-loader",
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "static" }],
+    }),
+    new VueLoaderPlugin(),
+  ],
+
+  devServer: {
+    host: "localhost",
+  },
+};
