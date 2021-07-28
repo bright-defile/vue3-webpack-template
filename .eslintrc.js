1e1c821142727ab1ad2c70c78d08a16f68db0021
bright-defile <raspberrypichef191@gmail.com>
2021-07-28 10:35:33 +0900

    eslintrc-edit

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,55 +10,55 @@ module.exports = {
     // 'plugin:vue/vue3-recommended', //lv3
     //js
     'eslint:recommended',
-    'plugin:vue/recommended',
-    // 'prettier',
     'plugin:prettier/recommended',
-    'plugin:prettier-vue/recommended',
   ],
-  // plugins: ['prettier'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-var': 2,
-    'prettier/prettier': 'error',
-    'vue/html-closing-bracket-newline': [
-      'error',
-      {
-        singleline: 'never',
-        multiline: 'never',
-      },
-    ],
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'always',
-          normal: 'never',
-          component: 'always',
-        },
-        svg: 'always',
-        math: 'always',
-      },
-    ],
-    'vue/html-indent': ['error', 2, {attribute: 2, closeBracket: 0}],
-    'vue/singleline-html-element-content-newline': [
-      'error',
-      {
-        ignoreWhenNoAttributes: true,
-        ignoreWhenEmpty: true,
-        ignores: ['pre', 'textarea', 'p', 'span', 'li'],
-      },
-    ],
-    'vue/max-attributes-per-line': [
-      'error',
-      {
-        singleline: 1,
-        multiline: {
-          max: 1,
-          allowFirstLine: true,
-        },
-      },
-    ],
+    'no-unused-vars': 1,
+    'prettier/prettier': ['error', {}, {usePrettierrc: true}],
+    'vue/html-closing-bracket-newline': 0,
+    // 'vue/html-closing-bracket-newline': [
+    //   'error',
+    //   {
+    //     singleline: 'never',
+    //     multiline: 'never',
+    //   },
+    // ],
+    // 'vue/html-self-closing': [
+    //   'error',
+    //   {
+    //     html: {
+    //       void: 'always',
+    //       normal: 'never',
+    //       component: 'always',
+    //     },
+    //     svg: 'always',
+    //     math: 'always',
+    //   },
+    // ],
+    'vue/max-attributes-per-line': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    // 'vue/html-indent': ['error', 2, { attribute: 2, closeBracket: 0 }],
+    // 'vue/singleline-html-element-content-newline': [
+    //   'error',
+    //   {
+    //     ignoreWhenNoAttributes: true,
+    //     ignoreWhenEmpty: true,
+    //     ignores: ['pre', 'textarea', 'p', 'span', 'li'],
+    //   },
+    // ],
+    // 'vue/max-attributes-per-line': [
+    //   'error',
+    //   {
+    //     singleline: 1,
+    //     multiline: {
+    //       max: 1,
+    //       allowFirstLine: true,
+    //     },
+    //   },
+    // ],
   },
 }
